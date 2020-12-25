86e6c5108d51a93dbcb84939b27852fe5ede964a
david-kariuki <dkaris.k@gmail.com>
2020-12-25 04:23:21 +0300

    Updated URLs to use new backend folder and file structure.

--- app/src/main/java/com/duesclerk/custom/network/NetworkUtils.java
+++ app/src/main/java/com/duesclerk/custom/network/NetworkUtils.java
@@ -2,43 +2,51 @@ package com.duesclerk.custom.network;
 
 public class NetworkUtils {
 
-    // This class holds all network urls and network tags needed by perform network requests
-    private static final String strProtocol = "https://";
-    private static final String stroke = "/";
-    private static final String strWebsiteMainDomain = strProtocol + "www.duesclerk.com" + stroke;
-    private static final String backendAndroidFolder = strWebsiteMainDomain + "andr" + stroke;
+    // This class holds all network urls and network tags needed to handle network requests
 
+    // Domain and Url constants
+    private static final String PROTOCOL            = "https://";
+    private static final String URL_SEPARATOR       = "/"; // Url separator
+    private static final String WEBSITE_URL         = PROTOCOL + "www.duesclerk.com"
+            + URL_SEPARATOR;
+    private static final String FOLDER_ANDROID      = WEBSITE_URL + "android" + URL_SEPARATOR;
+    private static final String FOLDER_APP          = FOLDER_ANDROID + "app" + URL_SEPARATOR;
+    private static final String FOLDER_VENDOR_NAME  = FOLDER_APP + "duesclerk" + URL_SEPARATOR;
+    private static final String FOLDER_CONTROLLERS  = FOLDER_VENDOR_NAME + "controllers"
+            + URL_SEPARATOR;
 
-    // Network urls
+    /**
+     * Network urls
+     */
 
     // URL to Signin user
-    public static final String URL_SIGNIN_USER = backendAndroidFolder + "SignInUser.php";
+    public static final String URL_SIGNIN_USER = FOLDER_CONTROLLERS + "signInUser.php";
 
     // URL to SignUp user
-    public static final String URL_SIGNUP_USER = backendAndroidFolder + "SignUpUser.php";
+    public static final String URL_SIGNUP_USER = FOLDER_CONTROLLERS + "signUpUser.php";
 
     // URL to fetch user profile details
-    public static final String URL_FETCH_USER_PROFILE_DETAILS = backendAndroidFolder
-            + "FetchUserProfile.php";
+    public static final String URL_FETCH_USER_PROFILE_DETAILS = FOLDER_CONTROLLERS
+            + "fetchUserProfile.php";
 
     // URL to update user profile details
-    public static final String URL_UPDATE_USER_PROFILE_DETAILS = backendAndroidFolder
-            + "UpdateUserProfile.php";
+    public static final String URL_UPDATE_USER_PROFILE_DETAILS = FOLDER_CONTROLLERS
+            + "updateUserProfile.php";
 
     // URL to Generate email verification code
-    public static final String URL_SEND_EMAIL_VERIFICATION_CODE = backendAndroidFolder
-            + "SendEmailVerificationCode.php";
+    public static final String URL_SEND_EMAIL_VERIFICATION_CODE = FOLDER_CONTROLLERS
+            + "sendEmailVerificationCode.php";
 
     // URL to verify email address
-    public static final String URL_VERIFY_EMAIL_ADDRESS = backendAndroidFolder
-            + "VerifyEmailAddress.php";
+    public static final String URL_VERIFY_EMAIL_ADDRESS = FOLDER_CONTROLLERS
+            + "verifyEmailAddress.php";
 
     // URL to reset password
-    public static final String URL_PASSWORD_RESET = backendAndroidFolder + "ResetPassword.php";
+    public static final String URL_PASSWORD_RESET = FOLDER_CONTROLLERS + "resetPassword.php";
 
     // URL to switch account type
-    public static final String URL_SWITCH_ACCOUNT_TYPE = backendAndroidFolder
-            + "SwitchAccountType.php";
+    public static final String URL_SWITCH_ACCOUNT_TYPE = FOLDER_CONTROLLERS
+            + "switchAccountType.php";
 
 
     // Tag used to cancel SignUp request
@@ -53,7 +61,8 @@ public class NetworkUtils {
             "TagRequestUpdateUserProfile";
 
     // Tag used to cancel user profile details request
-    public static final String TAG_FETCH_USER_PROFILE_STRING_REQUEST = "TagRequestFetchUserProfile";
+    public static final String TAG_FETCH_USER_PROFILE_STRING_REQUEST =
+            "TagRequestFetchUserProfile";
 
     // Tag used to cancel email verification code request
     public static final String TAG_SEND_EMAIL_VERIFICATION_STRING_REQUEST
