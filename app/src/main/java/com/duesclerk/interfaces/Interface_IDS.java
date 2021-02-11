360da0dd66725111180f5a02eb6c8af3764a51e8
david-kariuki <dkaris.k@gmail.com>
2021-02-11 22:16:57 +0300

    Interface to pass debts and contacts ids to where they are required

---
+++ app/src/main/java/com/duesclerk/interfaces/Interface_IDS.java
@@ -0,0 +1,21 @@
+package com.duesclerk.interfaces;
+
+/**
+ * This interface shares data between activities and adapters managing debts
+ */
+public interface Interface_IDS {
+
+    /**
+     * Interface to pass contacts ids to requiring activity for debts deletion
+     *
+     * @param contactsIds - Contact ids for debts deletion
+     */
+    void passContactsIds(String[] contactsIds);
+
+    /**
+     * Interface to pass debts ids to requiring activity for debts deletion
+     *
+     * @param debtsIds - Debt ids for debts deletion
+     */
+    void passDebtsIds(String[] debtsIds);
+}
