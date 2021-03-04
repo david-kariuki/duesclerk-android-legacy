c3e0206cca125e6b36ead20e01879b1ee1cb51fc
david-kariuki <dkaris.k@gmail.com>
2021-03-04 04:18:43 +0300

    Removed firstName, lastName and businessName keys

--- app/src/main/java/com/duesclerk/classes/custom_utilities/user_data/UserAccountUtils.java
+++ app/src/main/java/com/duesclerk/classes/custom_utilities/user_data/UserAccountUtils.java
@@ -9,9 +9,7 @@ public class UserAccountUtils {
     public static final String FIELD_USER_ID = "UserId";
 
     // Field names
-    public static final String FIELD_FIRST_NAME = "FirstName";
-    public static final String FIELD_LAST_NAME = "LastName";
-    public static final String FIELD_BUSINESS_NAME = "BusinessName";
+    public static final String FIELD_FULL_NAME_OR_BUSINESS_NAME = "FullName";
     public static final String KEY_NAME = "Name";
     public static final String FIELD_EMAIL_ADDRESS = "EmailAddress";
     public static final String FIELD_COUNTRY_NAME = "CountryName";
@@ -32,8 +30,6 @@ public class UserAccountUtils {
     // Account type
     public static final String FIELD_ACCOUNT_TYPE = "AccountType";
     public static final String FIELD_NEW_ACCOUNT_TYPE = "NewAccountType";
-    public static final String KEY_ACCOUNT_TYPE_PERSONAL = "AccountTypePersonal";
-    public static final String KEY_ACCOUNT_TYPE_BUSINESS = "AccountTypeBusiness";
 
     // Email Verification
     public static final String FIELD_EMAIL_VERIFIED = "EmailVerified";
