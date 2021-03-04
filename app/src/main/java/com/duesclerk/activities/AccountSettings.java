cfd40b8a4a3eb55c41a3038345141df073bb6630
david-kariuki <dkaris.k@gmail.com>
2021-03-04 04:16:22 +0300

    Removed switch account type feature

--- app/src/main/java/com/duesclerk/activities/AccountSettings.java
+++ app/src/main/java/com/duesclerk/activities/AccountSettings.java
@@ -2,28 +2,21 @@ package com.duesclerk.activities;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.duesclerk.R;
 import com.duesclerk.classes.custom_utilities.application.BroadCastUtils;
 import com.duesclerk.classes.custom_utilities.application.ViewsUtils;
-import com.duesclerk.classes.custom_utilities.user_data.DataUtils;
-import com.duesclerk.classes.custom_utilities.user_data.UserAccountUtils;
 import com.duesclerk.classes.custom_views.dialog_fragments.bottom_sheets.BottomSheetFragment_ChangePassword;
-import com.duesclerk.classes.custom_views.dialog_fragments.bottom_sheets.BottomSheetFragment_SwitchAccountType;
 import com.duesclerk.classes.storage_adapters.UserDatabase;
 
 public class AccountSettings extends AppCompatActivity {
 
     private Context mContext;
-    private String switchLabel;
-    private TextView textSwitchAccount;
     private BroadcastReceiver bcrSetActionText;
 
     @Override
@@ -33,12 +26,8 @@ public class AccountSettings extends AppCompatActivity {
 
         mContext = getApplicationContext(); // Initialize context object
 
-        // TextViews
-        textSwitchAccount = findViewById(R.id.textAccountSettings_SwitchAccount);
-
         // LinearLayouts
         LinearLayout llChangePassword = findViewById(R.id.llAccountSettings_ChangePassword);
-        LinearLayout llSwitchAccountType = findViewById(R.id.llAccountSettings_SwitchAccountType);
 
         // ImageViews
         ImageView imageExit = findViewById(R.id.imageAccountSettings_Exit);
@@ -51,25 +40,20 @@ public class AccountSettings extends AppCompatActivity {
         // Set switch account type action text
         setSwitchAccountTypeAction();
 
-        BottomSheetFragment_SwitchAccountType bottomSheetFragmentSwitchAccountType = new BottomSheetFragment_SwitchAccountType(
-                mContext, switchLabel);
-        bottomSheetFragmentSwitchAccountType.setRetainInstance(true);
-        bottomSheetFragmentSwitchAccountType.setCancelable(false);
-
         // Broadcast receiver
-        bcrSetActionText = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context arg0, Intent intent) {
-
-                String action = intent.getAction(); // Get action
-
-                if (action.equals(BroadCastUtils.bcrActionSetSwitchAccountTypeActionText)) {
-
-                    // Set switch account type action text
-                    setSwitchAccountTypeAction();
-                }
-            }
-        };
+//        bcrSetActionText = new BroadcastReceiver() {
+//            @Override
+//            public void onReceive(Context arg0, Intent intent) {
+//
+//                String action = intent.getAction(); // Get action
+//
+//                if (action.equals(BroadCastUtils.bcrActionSetSwitchAccountTypeActionText)) {
+//
+//                    // Set switch account type action text
+//                    setSwitchAccountTypeAction();
+//                }
+//            }
+//        };
 
         // Image exit onClick
         imageExit.setOnClickListener(v -> finish());
@@ -78,16 +62,6 @@ public class AccountSettings extends AppCompatActivity {
                 // Show change password fragment
                 ViewsUtils.showBottomSheetDialogFragment(getSupportFragmentManager(),
                         bottomSheetFragmentChangePassword, true));
-
-        llSwitchAccountType.setOnClickListener(v -> {
-
-                // Set switch account type action text
-                setSwitchAccountTypeAction();
-
-                // Show switch account type fragment
-                ViewsUtils.showBottomSheetDialogFragment(getSupportFragmentManager(),
-                        bottomSheetFragmentSwitchAccountType, true);
-        });
     }
 
     @Override
@@ -117,23 +91,23 @@ public class AccountSettings extends AppCompatActivity {
         // Get account type
         String accountType = database.getUserAccountInfo(null).get(0).getAccountType();
 
-        // Set switch account label
-        if (accountType.equals(UserAccountUtils.KEY_ACCOUNT_TYPE_PERSONAL)) {
-            // Personal account
-
-            // Set switch label for bottom sheet title
-            switchLabel = DataUtils.getStringResource(mContext, R.string.hint_business_account);
-
-        } else if (accountType.equals(UserAccountUtils.KEY_ACCOUNT_TYPE_BUSINESS)) {
-            // Business account
-
-            // Set switch label for bottom sheet title
-            switchLabel = DataUtils.getStringResource(mContext, R.string.hint_personal_account);
-        }
+//        // Set switch account label
+//        if (accountType.equals(UserAccountUtils.KEY_ACCOUNT_TYPE_PERSONAL)) {
+//            // Personal account
+//
+//            // Set switch label for bottom sheet title
+//            switchLabel = DataUtils.getStringResource(mContext, R.string.hint_business_account);
+//
+//        } else if (accountType.equals(UserAccountUtils.KEY_ACCOUNT_TYPE_BUSINESS)) {
+//            // Business account
+//
+//            // Set switch label for bottom sheet title
+//            switchLabel = DataUtils.getStringResource(mContext, R.string.hint_personal_account);
+//        }
 
         // Set menu action label
-        textSwitchAccount.setText(DataUtils.getStringResource(mContext,
-                R.string.action_switch_account_type, switchLabel
-        ));
+//        textSwitchAccount.setText(DataUtils.getStringResource(mContext,
+//                R.string.action_switch_account_type, switchLabel
+//        ));
     }
 }
