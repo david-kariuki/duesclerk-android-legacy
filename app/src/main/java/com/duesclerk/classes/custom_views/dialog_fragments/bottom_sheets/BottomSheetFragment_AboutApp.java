0fa67b14728a977326e858ef52c1c040a481cf44
david-kariuki <dkaris.k@gmail.com>
2021-03-10 18:18:16 +0300

    Added BottomSheet to show about application information

---
+++ app/src/main/java/com/duesclerk/classes/custom_views/dialog_fragments/bottom_sheets/BottomSheetFragment_AboutApp.java
@@ -0,0 +1,96 @@
+package com.duesclerk.classes.custom_views.dialog_fragments.bottom_sheets;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.duesclerk.R;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import org.jetbrains.annotations.NotNull;
+
+@SuppressWarnings("rawtypes")
+@SuppressLint("ValidFragment")
+public class BottomSheetFragment_AboutApp extends BottomSheetDialogFragment {
+
+    private BottomSheetBehavior bottomSheetBehavior;
+    private BottomSheetBehavior.BottomSheetCallback bottomSheetCallback;
+
+    public BottomSheetFragment_AboutApp() {
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final BottomSheetDialog dialog = (BottomSheetDialog)
+                super.onCreateDialog(savedInstanceState);
+
+        View contentView = View.inflate(getContext(), R.layout.bottom_sheet_about_app, null);
+
+        // ImageViews
+        ImageView imageDismiss = contentView.findViewById(R.id.imageAboutApp_Dismiss);
+
+        // TextViews
+        TextView textAboutMessage = contentView.findViewById(R.id.textAppAbout_AboutMessage);
+
+        // Set scrolling movement method
+        textAboutMessage.setMovementMethod(new ScrollingMovementMethod());
+
+        // Dismiss onClick
+        imageDismiss.setOnClickListener(v -> dismiss());
+
+        // Set BottomSheet callback
+        this.bottomSheetCallback = new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_DRAGGING) bottomSheetBehavior
+                        .setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+            }
+        };
+
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE); // Remove window title
+
+        // Set transparent background
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        // Set Custom View To Dialog
+        dialog.setContentView(contentView);
+
+        // Set BottomSheet behaviour
+        this.bottomSheetBehavior = BottomSheetBehavior.from((View) contentView.getParent());
+
+        // Set dialog transparent background
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        return dialog;
+    }
+
+    @Override
+    public void onAttach(@NotNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        this.bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        this.bottomSheetBehavior.addBottomSheetCallback(bottomSheetCallback);
+    }
+}
