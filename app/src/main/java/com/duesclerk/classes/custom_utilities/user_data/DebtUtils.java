3867ddd556b4f6e9f2c978274ab89ba8439f9e6c
david-kariuki <dkaris.k@gmail.com>
2021-03-25 03:55:27 +0300

    Added FIELD_DEBT_DATE_TIME_ADDED constant

--- app/src/main/java/com/duesclerk/classes/custom_utilities/user_data/DebtUtils.java
+++ app/src/main/java/com/duesclerk/classes/custom_utilities/user_data/DebtUtils.java
@@ -5,6 +5,7 @@ public class DebtUtils {
     // Field names
     public static final String FIELD_DEBT_ID = "DebtId";
     public static final String FIELD_DEBT_AMOUNT = "DebtAmount";
+    public static final String FIELD_DEBT_DATE_TIME_ADDED = "DebtDateTimeAdded";
     public static final String FIELD_DEBT_DATE_ISSUED = "DebtDateIssued";
     public static final String FIELD_DEBT_DATE_DUE = "DebtDateDue";
     public static final String FIELD_DEBT_DESCRIPTION = "DebtDescription";
